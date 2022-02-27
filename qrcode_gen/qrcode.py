f7d3cb84276488f24a912f5d69ee8280d3500bfb
Imad Abdou <igentle.appletec@gmail.com>
2022-02-27 10:18:35 +0100

    increase qality

--- qrcode_gen/qrcode.py
+++ qrcode_gen/qrcode.py
@@ -8,4 +8,4 @@ import pyqrcode
 def gen_qrcode(text):
     data = pyqrcode.create(text)
 
-    return f'data:image/png;base64,{data.png_as_base64_str(scale=5)}'
+    return f'data:image/png;base64,{data.png_as_base64_str(scale=8)}'
